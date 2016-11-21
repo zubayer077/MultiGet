98aea3693d5ee8a5819f16bc1d54617d96676414
zubayer077 <zubayer077@gmail.com>
2016-11-20 20:10:30 -0500

    Adding Files

---
+++ api/MultiGet.js
@@ -0,0 +1,65 @@
+var path = require("path"),
+	request = require('request'),
+	fs = require('fs');
+
+/**
+*	this class takes url, filename, filesize, number of chunks as parameter
+*	downloads chunks of file and merge them
+*	@url: an url in the format http://XXXXX.XXX/XXX
+*	@fileName: a filename in String
+*	@fileSize: a file size in Byes, Number
+*	@numberOfChunks: Numbers of chunks the file has to be downloaded, Number
+*	@rturn: undefined
+**/
+
+function MultiGet(url,fileName,fileSize,numberOfChunks){
+
+	/* Gets chunks of the file sequentially*/
+	function getParts(url, fileName,fileSize,numberOfChunks){
+
+		var chunkSize = Math.floor(fileSize/numberOfChunks),
+			loopIndex = 0,
+			fileStartsAt=0;
+
+		/* Makes a GET request for each chunk of file*/
+		(function getChunk(fileStartsAt, fileEndsAt){
+			request(
+					{
+						method: 'GET',
+					    uri: url,
+					    headers: {
+		    				'Range': "bytes="+fileStartsAt+"-"+fileEndsAt
+		  				}
+					}, function (error, response, body) {
+					  if (!error && response.statusCode == 206) {			
+						++loopIndex;
+						if (loopIndex<numberOfChunks) {
+
+							var lastExtraBytes = 0;
+							if ((loopIndex+1)>=numberOfChunks && (fileSize-1)-(fileEndsAt+chunkSize)>0){
+									lastExtraBytes= (fileSize-1)-(fileEndsAt+chunkSize);
+							}
+
+							getChunk(fileEndsAt+1, fileEndsAt+lastExtraBytes+chunkSize);
+						} else {
+							process.stdout.write("Filename "+fileName+" is downloaded \n");
+						}
+					  }
+		  
+				}).pipe(fs.createWriteStream(fileName, {'flags': 'a'}));
+		})(fileStartsAt, (fileStartsAt+chunkSize)-1);
+	}
+
+	/* Public Consctructor*/
+	this.constructor = function(url, fileName,fileSize,numberOfChunks){
+		getParts(url, fileName,fileSize,numberOfChunks);
+	};
+	this.constructor(url, fileName,fileSize,numberOfChunks);
+};
+
+/**
+*	returns an instance of MultiGet class
+*/
+module.exports = function(url,fileName,fileSize,numberOfChunks){
+	return new MultiGet(url,fileName,fileSize,numberOfChunks);
+};
---
+++ app.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+var path = require("path"),
+	multiGet = require(path.join(__dirname, "api", "MultiGet"));
+	program = require('commander');
+
+/**
+*	This class parses the command line arguments
+*	calls multiGet to downloaed and merge all the chunks
+*/
+
+/* Default values*/
+var OUTPUT_FILENAME = "default",
+	OUTPUT_FILESIZE = 4194304,
+	NUMBER_OF_CHUNKS = 4;
+
+/**
+*	processArguments function parses arguments passed from
+*	sets the optional arguments
+*	call multiGet method to download chunks of file and merge them
+*/
+(function processArguments(arguments){
+	/*parse command line inputes*/
+	program
+		.option('-o <string>', 'Write output to <file> instead of default (optional)')
+		.option('-s, --size <Number>', 'File download size in MB (optional)', parseInt)
+		.option('-c, --chunks <Number>', 'Number of chunks (optional)', parseInt)
+		.usage('[options] <url>')
+		.parse(arguments);
+
+
+	/*regular expression for checking if a valid url is passed*/
+	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
+		urlRegex = new RegExp(expression);
+
+
+	/*
+	*	This code below checks for the correctess of arguments.
+	*	First it checks if an url is passed as argument, then it checks whether the url is lexically correct
+	*	then sets the optional values from the ones passed from the command line
+	*	Note that command line values are sanitized when they are parsed with appropritate parser
+	*	
+	*	this calls multiGet to Get the chunks of file and merge them
+	*	After this step, it is expected that the arguments passed to multiGet has a proper value
+	*/
+	if (!program.args.length) {
+		program.help();
+	} else if (urlRegex.test(program.args[0])){
+
+		if (program.O) OUTPUT_FILENAME = ""+program.O;
+		if (program.size) {
+			OUTPUT_FILESIZE = program.size *1024*1024;
+		}
+		if (program.chunks && program.chunks<OUTPUT_FILESIZE){
+			NUMBER_OF_CHUNKS = program.chunks;
+		}
+
+		multiGet(program.args[0], OUTPUT_FILENAME, OUTPUT_FILESIZE, NUMBER_OF_CHUNKS);
+	} else {
+		process.stdout.write("A valid URL is required \n");
+	}
+})(process.argv);
